7ba049e8b2c39a71e1fe2b5dce469d45a1f1b5b6
Diss <65290988+Dissurender@users.noreply.github.com>
2023-09-07 09:51:07 -0500

    init commit

---
+++ src/main/java/com/jetlogged/jetlog/JetLogApplication.java
@@ -0,0 +1,13 @@
+package com.jetlogged.jetlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JetLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jetlogged/jetlog/JetLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetlogged.jetlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JetLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
